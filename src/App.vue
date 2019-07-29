3818cc95aca96946d27f910d403572f495c9394d
codewiz <c..chaiwat.ch@gmail.com>
2019-07-29 13:57:04 +0700

    finish project shopping

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
     <div class="container">
         <div class="row">
             <div class="col-md-7">
-							<div class="row">
-								<div :key="product.id" class="col-md-6" v-for="product in products">
-									<Product 
-										:product="product"
-										v-on:add-to-card="addToCart(product)">
-									</Product>
-								</div>
-							</div>
-						</div>
-            <div class="col-md-5"></div>
+				<div class="row">
+					<div :key="product.id" class="col-md-6" v-for="product in products">
+						<Product
+							:product="product"
+							:isInCart="isInCart(product)"
+							v-on:add-to-card="addToCart(product)">
+						</Product>
+					</div>
+				</div>
+			</div>
+            <div class="col-md-5 my-5">
+				<Cart
+					:items="cart"
+					v-on:pay="pay()"
+                    v-on:remove-from-cart="removeFromCart($event)">
+				</Cart>
+			</div>
         </div>
     </div>
 </template>
@@ -19,11 +26,13 @@
 <script>
 import products from '@/products.json'
 import Product from '@/components/Product.vue'
+import Cart from '@/components/Cart.vue'
 
 export default {
-  name: 'app',
+	name: 'app',
 	components: {
-		Product
+		Product,
+		Cart
 	},
 	data() {
 		return {
@@ -33,7 +42,21 @@ export default {
 	},
 	methods: {
 		addToCart(product) {
-			console.log(product)	
+			this.cart.push(product)
+		},
+		isInCart(product) {
+			const item = this.cart.find(item => item.id === product.id)
+			if(item) {
+				return true
+			}
+			return false
+		},
+		removeFromCart(product) {
+			this.cart = this.cart.filter(item => item.id !== product.id)
+		},
+		pay() {
+			this.cart = []
+			alert('Shopping completed !')
 		}
 	}
 }
